2a05b4e88fa4ad6b005a392ea4140e7f14ed0663
jk872001 <jiteshkumar872001@gmail.com>
2023-10-27 12:27:41 +0530

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,31 @@
-import express from "express";
+import express, { NextFunction,Request, Response} from "express";
+import createHttpError, { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app=express()
 
+
+// for checking error handling
+// app.get("/",(req,res)=>
+// {
+//      const newErr=createHttpError(401,"You are not authorized");
+//      throw newErr
+// })
+
+// global error handling middleware
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>
+{
+    logger.error(err.message)
+    const statusCode= err.statusCode || 500;
+    res.status(statusCode).json({
+        errors:[
+            {
+                type:err.name,
+                msg:err.message,
+                path:"",
+                location:""
+            }
+        ]
+    })
+})
 export default app;
