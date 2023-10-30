8d857c1a2a0328dc63d9a1c3423e801ce539f6ec
jk872001 <jiteshkumar872001@gmail.com>
2023-10-30 22:25:27 +0530

    add test case "should return 400 status code if email is already present"

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,7 @@
 import { Repository } from "typeorm";
 import { User } from "../entity/User";
 import { UserData } from "../types";
+import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
 import { Roles } from "../constants";
 
@@ -8,13 +9,24 @@ export class UserService {
    constructor(private userRepository: Repository<User>) {}
 
    async create({ firstName, lastName, email, password }: UserData) {
+      const existingUser = await this.userRepository.findOne({
+         where: { email: email },
+      });
+      if (existingUser) {
+         const error = createHttpError(400, "Email Already exists");
+         throw error;
+      }
+
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+
       try {
          await this.userRepository.save({
             firstName,
             lastName,
             email,
-            password,
-            role:Roles.CUSTOMER
+            password: hashedPassword,
+            role: Roles.CUSTOMER,
          });
       } catch (err) {
          const error = createHttpError(500, "failed to store data in db");
@@ -23,6 +35,4 @@ export class UserService {
    }
 }
 
-
-
-// 500 is the database error 
+// 500 is the database error
--- tests/user/register.spec.ts
+++ tests/user/register.spec.ts
@@ -1,4 +1,4 @@
-import {  DataSource } from "typeorm";
+import { DataSource } from "typeorm";
 import app from "../../src/app";
 import request from "supertest";
 import { AppDataSource } from "../../src/config/data-source";
@@ -6,31 +6,26 @@ import { User } from "../../src/entity/User";
 import { Roles } from "../../src/constants";
 // import { truncateTable } from "../utils";
 
-
-
 describe("POST /auth/register", () => {
-    let connection:DataSource
+   let connection: DataSource;
 
-    // it will help in clearing the database everytime as it is imp that db is empty for every particular test case.
+   // it will help in clearing the database everytime as it is imp that db is empty for every particular test case.
 
-    beforeAll(async()=>
-    {
-        connection= await AppDataSource.initialize()
-    })
+   beforeAll(async () => {
+      connection = await AppDataSource.initialize();
+   });
 
-    beforeEach(async()=>
-    {
-        // database truncate
-         // await truncateTable(connection)
+   beforeEach(async () => {
+      // database truncate
+      // await truncateTable(connection)
 
-         await connection.dropDatabase();
-         await connection.synchronize()
-    })
+      await connection.dropDatabase();
+      await connection.synchronize();
+   });
 
-    afterAll(async()=>
-    {
-       await connection.destroy()
-    })
+   afterAll(async () => {
+      await connection.destroy();
+   });
 
    describe("Given all fields", () => {
       it("should return 201 status code", async () => {
@@ -76,17 +71,14 @@ describe("POST /auth/register", () => {
             password: "123432",
          };
          // Act
-         await request(app)
-            .post("/auth/register")
-            .send(userData);
+         await request(app).post("/auth/register").send(userData);
          // Assert
-          const userRepository= connection.getRepository(User)
-          const users= await userRepository.find()
-          expect(users).toHaveLength(1)
-          expect(users[0].firstName).toBe(userData.firstName)
-          expect(users[0].lastName).toBe(userData.lastName)
-          expect(users[0].email).toBe(userData.email)
-
+         const userRepository = connection.getRepository(User);
+         const users = await userRepository.find();
+         expect(users).toHaveLength(1);
+         expect(users[0].firstName).toBe(userData.firstName);
+         expect(users[0].lastName).toBe(userData.lastName);
+         expect(users[0].email).toBe(userData.email);
       });
 
       it("should assign role customer", async () => {
@@ -98,18 +90,52 @@ describe("POST /auth/register", () => {
             password: "123432",
          };
          // Act
-         await request(app)
+         await request(app).post("/auth/register").send(userData);
+         // Assert
+         const userRepository = connection.getRepository(User);
+         const users = await userRepository.find();
+         expect(users[0].role).toBe(Roles.CUSTOMER);
+      });
+
+      it("should store hashed password in database", async () => {
+         // Arrange
+         const userData = {
+            firstName: "Jitesh",
+            lastName: "Kumar",
+            email: "jiteshkumar@gmail.com",
+            password: "123432",
+         };
+         // Act
+         await request(app).post("/auth/register").send(userData);
+         // Assert
+         const userRepository = connection.getRepository(User);
+         const users = await userRepository.find();
+         expect(users[0].password).not.toBe(userData.password);
+         expect(users[0].password).toHaveLength(60);
+         //   one more expect is need to write
+      });
+      it("should return 400 status code if email is already present", async () => {
+         // Arrange
+         const userData = {
+            firstName: "Jitesh",
+            lastName: "Kumar",
+            email: "jiteshkumar@gmail.com",
+            password: "123432",
+         };
+         // we have to store the data in the db for this test case
+         const userRepository = connection.getRepository(User);
+         await userRepository.save({ ...userData, role: Roles.CUSTOMER });
+         // Act
+         const response = await request(app)
             .post("/auth/register")
             .send(userData);
          // Assert
-          const userRepository= connection.getRepository(User)
-          const users= await userRepository.find()
-          expect(users[0].role).toBe(Roles.CUSTOMER)
-         
-
+         const users = await userRepository.find();
+         expect(response.statusCode).toBe(400);
+         expect(users).toHaveLength(1);
       });
 
-      it.todo("should return id ")
+      it.todo("should return id ");
    });
    describe("Given all fields", () => {});
 });
